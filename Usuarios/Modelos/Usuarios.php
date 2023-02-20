673ac6179dcdc2b4f42458a47d79d2bebf1b0ae3
Alejandro Burbano <Julianbur16@gmail.com>
2023-02-20 03:28:46 -0500

    conectando con mysql

--- Usuarios/Modelos/Usuarios.php
+++ Usuarios/Modelos/Usuarios.php
@@ -1,9 +1,29 @@
 <?php
 
-class Usuarios{
+require_once("../../conexion.php");
+session_start();
+class Usuarios extends conection{
+    
+    public function __construct(){
 
+        $this->db=parent::validar();
+    }
 
+    public function login($usuario, $password){
+        $statement=$this->db->prepare("SELECT * FROM usuarios WHERE USUARIO=:usuario AND PASSWORD=:password");
+        $statement->bindParam(":usuario",$usuario);
+        $statement->bindParam(":usuario",$usuario);
+        $statement->execute();
 
+        if($statement->rowCount()==1){
+            $result=$statement->fetch();
+            $_SESSION["Nombre"]=$result["Nombre"]."".$result["Apellido"];
+            $_SESSION["Id"]=$result["Id"];
+            $_SESSION["Perfil"]=$result["Perfil"];
+            return true;
+        }
+        return false;
+    }
     
 }
 
